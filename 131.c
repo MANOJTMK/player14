c8502595e9cad890714c9b5a4b04eeb08ae97af4
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-03-17 16:53:45 +0530

    Create 131.c

---
+++ 131.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int n,rem,i,sum=0;
+    clrscr();
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        if(rem%2!=0)
+        {
+            sum=sum+rem;
+        }
+        n=n/10;
+    }
+    if(sum%2==0)
+    {
+        printf("E");
+    }
+    else
+    {
+        printf("O");
+    }
+    getch();
+}
