74621a5996c50879b5c26544add93e9540c30f38
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-03-24 07:45:29 +0530

    Create 140.c

---
+++ 140.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    
+char s[10000];
+int l,i,n;
+scanf("%[^\n]",s);
+for(l=0;s[l]!='\0';l++);
+for(i=0;i<l;i++)
+{
+    if(s[i]=='a'||s[i]=='b')
+    {
+        
+        
+    }
+      else
+      {
+          break;
+      }
+}
+if(i==l)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+
+    return 0;
+}
