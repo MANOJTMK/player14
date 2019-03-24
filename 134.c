a91912c5a33cd76001cb4c5336d32378887caced
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-03-24 07:36:38 +0530

    Create 134.c

---
+++ 134.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int a[1000],n,i,min,c,b;
+    
+    scanf("%d %d %d",&n,&c,&b);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=c;
+    for(i=0;i<=n;i++)
+    {
+        if((a[i]>=c)&&(a[i]<=b))
+        {
+            if(min>a[i])
+            {
+                min=a[i];
+            }
+        }
+    }
+    
+    printf("%d",min);
+    return 0;
+}
