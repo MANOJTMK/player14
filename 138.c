0a2e79c22b802983b62851e60756656a51ff0f08
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-03-24 07:43:17 +0530

    Create 138.c

---
+++ 138.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,n,l=0,r=0;
+    scanf("%d",&n);
+    while(n>=1)
+    {
+        if(n%2==0)
+        {
+            n=n/2;
+            l++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if(n==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
